2c3438a8b8192780e27da3d7814cafac9c70d4c6
joao_lima <joaocsantoslima@gmail.com>
2024-05-21 21:11:31 -0300

    contador

---
+++ bp.py
@@ -0,0 +1,11 @@
+# importa as bibliotecas
+import os
+import time
+
+# usuário informa um número
+contador = int(input('Informe um numero inteiro:'))
+
+# conta a partir do número informado até 0
+while contador >= 0:
+    print(contador)
+    contador -= 1
