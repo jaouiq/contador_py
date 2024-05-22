3824988546ed164ef2df328e36793b5d1d768cff
joao_lima <joaocsantoslima@gmail.com>
2024-05-21 21:16:42 -0300

    contador2

--- bp.py
+++ bp.py
@@ -4,8 +4,13 @@ import time
 
 # usuário informa um número
 contador = int(input('Informe um numero inteiro:'))
+os.system('cls')
 
 # conta a partir do número informado até 0
 while contador >= 0:
-    print(contador)
+    print(f'Contagem regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
+
+print('GAME OVER')
